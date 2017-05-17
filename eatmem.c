ea8a14c6d745fa269c9822b3cfb2234846d77e8b
Yuan J. Xu <yxu@ib16b08.eng.platformlab.ibm.com>
2017-05-17 04:44:08 -0400

    first version

---
+++ eatmem.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    char *p=NULL;
+int i=0;
+ //   p=(char*)malloc(512*1024*1024);
+    p=(char*)malloc(500*1024*1024);
+//    for(i=0;i<512*1024*1024 && p !=NULL ;i++,p++)
+    for(i=0;i<500*1024*1024 && p !=NULL ;i++,p++)
+        *p=2*4;
+        sleep(500);
+}
